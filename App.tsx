3543bb7d089e8b06c3352823bf4cecb55041d0fb
Burcu Ozdemir <burcuhimmet1@gmail.com>
2023-07-29 15:56:48 +0300

    Title and user story component added

---
+++ App.tsx
@@ -0,0 +1,93 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
+import Title from './components/Title/Title';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import style from './assets/styles/main';
+import UserStory from './components/UserStory/UserStory';
+
+const App = () => {
+  //All of the items in our stories
+  const data = [
+    {
+      firstName: 'Joseph',
+      id: 1,
+    },
+    {
+      firstName: 'Angel',
+      id: 2,
+    },
+    {
+      firstName: 'White',
+      id: 3,
+    },
+    {
+      firstName: 'Olivier',
+      id: 4,
+    },
+    {
+      firstName: 'nata',
+      id: 5,
+    },
+    {
+      firstName: 'Adam',
+      id: 6,
+    },
+    {
+      firstName: 'Sean',
+      id: 7,
+    },
+    {
+      firstName: 'Nicolas',
+      id: 8,
+    },
+    {
+      firstName: 'Joe',
+      id: 9,
+    },
+  ];
+  const pageSize = 4;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [renderedData, setRenderedData] = useState([]);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={style.header}>
+          <Title title="Let’s Explore" />
+          <Pressable style={style.messageIcon}>
+            <FontAwesomeIcon icon={faEnvelope} color={'#CACDDE'} size={20} />
+            <View style={style.messageNumberContainer}>
+              <Text style={style.messageNumber}>2</Text>
+            </View>
+          </Pressable>
+        </View>
+        <View style={style.userStoryContainer}>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            data={data}
+            renderItem={({item}) => <UserStory firstName={item.firstName} />}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ components/UserStory/style.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  storyContainer: {
+    marginRight: 20,
+  },
+  name: {
+    textAlign: 'center',
+    fontFamily: 'Inter',
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#022150',
+    lineHeight: 17,
+    marginTop: 8,
+  },
+  userImageContainer: {
+    borderWidth: 1,
+    borderColor: '#F35BAC',
+    padding: 3,
+    borderRadius: 50,
+  },
+});
+
+export default style;
